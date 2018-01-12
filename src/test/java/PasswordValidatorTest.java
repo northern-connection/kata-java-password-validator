556b1866ada70df930618ded8a2e5691a5dd9170
Antonio de la Torre <antonio@codesai.com>
2018-01-12 09:59:54 +0100

    Red: no uppercase

--- src/test/java/PasswordValidatorTest.java
+++ src/test/java/PasswordValidatorTest.java
@@ -1,7 +1,6 @@
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class PasswordValidatorTest {
 
@@ -12,5 +11,13 @@ public class PasswordValidatorTest {
 
         assertTrue(validator.validate(password));
 	}
+
+    @Test
+    public void should_fail_when_no_capital_letter() {
+        String password = "aa_123456";
+        PasswordValidator validator = new PasswordValidator();
+
+        assertFalse(validator.validate(password));
+    }
 
 }
