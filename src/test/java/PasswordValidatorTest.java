3950494c6862324e6290153e20108e435584ad49
Antonio de la Torre <antonio@codesai.com>
2018-01-12 10:10:53 +0100

    Red: no underscore

--- src/test/java/PasswordValidatorTest.java
+++ src/test/java/PasswordValidatorTest.java
@@ -31,4 +31,11 @@ public class PasswordValidatorTest {
 
         assertFalse(validator.validate(password));
     }
+
+    @Test
+    public void should_fail_when_no_underscore() {
+        String password = "Aa0123456";
+
+        assertFalse(validator.validate(password));
+    }
 }
