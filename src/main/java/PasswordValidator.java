659541ce18217ab01c33854d3a57acc1f655d910
Antonio de la Torre <antonio@codesai.com>
2018-01-12 10:00:44 +0100

    Green: no lowercase

--- src/main/java/PasswordValidator.java
+++ src/main/java/PasswordValidator.java
@@ -1,6 +1,7 @@
 public class PasswordValidator {
     public Boolean validate(String password) {
-        return password.toLowerCase() != password;
+        return password.toLowerCase() != password
+                && password.toUpperCase() != password;
     }
 }
 
