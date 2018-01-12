3b7964f0807892a432e0af0fa338a2ac113150c7
Antonio de la Torre <antonio@codesai.com>
2018-01-12 10:08:59 +0100

    Refactor

--- src/main/java/PasswordValidator.java
+++ src/main/java/PasswordValidator.java
@@ -1,7 +1,18 @@
-public class PasswordValidator {
-    public Boolean validate(String password) {
-        return password.toLowerCase() != password
-                && password.toUpperCase() != password;
+import java.util.Objects;
+
+class PasswordValidator {
+
+    Boolean validate(String password) {
+        return hasUpperCase(password)
+                && hasLowerCase(password);
+    }
+
+    private boolean hasLowerCase(String password) {
+        return !Objects.equals(password.toUpperCase(), password);
+    }
+
+    private boolean hasUpperCase(String password) {
+        return !Objects.equals(password.toLowerCase(), password);
     }
 }
 
--- src/test/java/PasswordValidatorTest.java
+++ src/test/java/PasswordValidatorTest.java
@@ -1,13 +1,19 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class PasswordValidatorTest {
+    private PasswordValidator validator;
+
+    @Before
+    public void before() {
+        validator = new PasswordValidator();
+    }
 
 	@Test
 	public void should_validate_a_correct_password() {
         String password = "Aa_123456";
-        PasswordValidator validator = new PasswordValidator();
 
         assertTrue(validator.validate(password));
 	}
@@ -15,7 +21,6 @@ public class PasswordValidatorTest {
     @Test
     public void should_fail_when_no_capital_letter() {
         String password = "aa_123456";
-        PasswordValidator validator = new PasswordValidator();
 
         assertFalse(validator.validate(password));
     }
@@ -23,9 +28,7 @@ public class PasswordValidatorTest {
     @Test
     public void should_fail_when_no_lower_case() {
         String password = "AA_123456";
-        PasswordValidator validator = new PasswordValidator();
 
         assertFalse(validator.validate(password));
     }
-
 }
